276b05f58a2837eeb5ac5c0a04ec8aff2ce2c8ff
flame6672 <gustavoa3498@gmail.com>
2026-08-24 08:35:58 -0400

    .

---
+++ Progreso/Condicionales.java
@@ -0,0 +1,23 @@
+package Progreso;
+
+import java.util.Scanner;
+
+public class Condicionales {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese su nota: ");
+        int nota = input.nextInt();
+        
+
+        if (nota < 4) {
+            System.out.println("Nota insuficiente");
+        }
+        else if (nota > 5) {
+            System.out.println("Muy buena nota");
+        }
+        else {
+            System.out.println("Nota aprobada");
+        }
+
+        }
+    }
