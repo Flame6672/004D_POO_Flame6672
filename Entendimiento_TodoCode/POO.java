6cf0122b0af71ec6becdbad7bb7bfa5bf1334c64
flame6672 <gustavoa3498@gmail.com>
2026-09-05 18:39:24 -0400

    .

--- Entendimiento_TodoCode/POO.java
+++ Entendimiento_TodoCode/POO.java
@@ -4,9 +4,21 @@ public class POO {
     public static void main(String[] args) {
 
     Alumno a1 = new Alumno();
-    Alumno a2 = new Alumno(001, "Juan", "Barrientos", 18);
+    Alumno a2 = new Alumno(002, "Eustaquio", "Barrientos", 18);
+    System.out.println("La id del alumno 2 es: " + a2.getId());
+    System.out.println("El nombre del alumno es: " + a2.getNombre());
+    System.out.println("El apellido del alumno es: " + a2.getApellido());
+    System.out.println("La edad del alumno es: " + a2.getEdad());
+    System.out.println("======================================");
 
-    System.out.println("La id de el alumno 1 es: " + a1.getId());
-    System.out.println("La id de el alumno 2 es: " + a2.getId());
+    a1.setId(1);
+    a1.setNombre("Pepito");
+    a1.setApellido("Gatete");
+    a1.setEdad(10);
+
+    System.out.println("La id del alumno 1 es: " + a1.getId());
+    System.out.println("El nombre del alumno es: " + a1.getNombre());
+    System.out.println("El apellido del alumno es: " + a1.getApellido());
+    System.out.println("La edad del alumno es: " + a1.getEdad());
     }
 }
